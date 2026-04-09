60e451ace0ab783e29bcf73e0585e352ff912f4b
Assassin8923 <c.hryniuk.rostyslav@student.uzhnu.edu.ua>
2026-04-09 23:40:11 +0300

    Add project files.

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Sonic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // При запуску одразу відкриваємо сторінку діагностики
+            MainFrame.Navigate(new DashboardPage());
+        }
+
+        private void BtnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new DashboardPage());
+        }
+
+        private void BtnCleaner_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CleanerPage());
+        }
+
+        private void ButtonD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
